e2083cd6cc00daadc9a9430595b24737588acd7d
Learner0000 <lichenglong0410@outlook.com>
2020-07-13 13:47:14 +0800

    初始化项目

---
+++ src/main/java/com/citydo/lcl/Domain/MysqlTb.java
@@ -0,0 +1,5 @@
+package com.citydo.lcl.Domain;
+
+public class MysqlTb {
+
+}
---
+++ src/test/java/com/citydo/lcl/LclApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citydo.lcl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LclApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
