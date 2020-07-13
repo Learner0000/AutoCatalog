c6f503c45521cb6d70a1ecbd86f8b9aa6108f8e4
Learner0000 <lichenglong0410@outlook.com>
2020-07-13 16:54:24 +0800

    元数据定义

--- src/main/java/com/citydo/lcl/Domain/MysqlTb.java
+++ src/main/java/com/citydo/lcl/Domain/MysqlTb.java
@@ -1,5 +1,50 @@
 package com.citydo.lcl.Domain;
 
 public class MysqlTb {
+    private String tbName;
+    private String tbDesc;
+    private String schema;
+    private String schemaDesc;
 
+    public String getTbName() {
+        return tbName;
+    }
+
+    public void setTbName(String tbName) {
+        this.tbName = tbName;
+    }
+
+    public String getTbDesc() {
+        return tbDesc;
+    }
+
+    public void setTbDesc(String tbDesc) {
+        this.tbDesc = tbDesc;
+    }
+
+    public String getSchema() {
+        return schema;
+    }
+
+    public void setSchema(String schema) {
+        this.schema = schema;
+    }
+
+    public String getSchemaDesc() {
+        return schemaDesc;
+    }
+
+    public void setSchemaDesc(String schemaDesc) {
+        this.schemaDesc = schemaDesc;
+    }
+
+    @Override
+    public String toString() {
+        return "MysqlTb{" +
+                "tbName='" + tbName + '\'' +
+                ", tbDesc='" + tbDesc + '\'' +
+                ", schema='" + schema + '\'' +
+                ", schemaDesc='" + schemaDesc + '\'' +
+                '}';
+    }
 }
